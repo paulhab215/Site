4e6fc7621dfe452b22e61f7bcadf1c5d68f1c139
paulhab215 <paulhab39@gmail.com>
2018-02-27 22:10:13 -0600

    dropped overlay for header and footer

--- penyler/index.php
+++ penyler/index.php
@@ -8,7 +8,7 @@
 <link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
 </head>
 <body id="top">
-<div class="bgded overlay" style="background-image:url('images/backgrounds/blackfloral.jpg');"> 
+<div class="bgded" style="background-image:url('images/backgrounds/blackfloral.jpg');"> 
   <div class="wrapper">
     <header id="header" class="hoc clear">
       <div id="logo"> 
@@ -87,7 +87,7 @@
     <div class="clear"></div>
   </main>
 </div>
-<div class="bgded overlay" style="background-image:url('images/backgrounds/blackfloral.jpg');">
+<div class="bgded" style="background-image:url('images/backgrounds/blackfloral.jpg');">
   <footer id="footer" class="hoc clear center"> 
     <h3 class="heading uppercase">Habjanetz</h3>
     <ul class="faico clear">
