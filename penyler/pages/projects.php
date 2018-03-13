432bf1f0e738e8b05e729b72b346b768cd330125
Paul Habjanetz <paulhab39@gmail.com>
2018-03-12 20:04:08 -0500

    Updated spacing of html

--- penyler/pages/projects.php
+++ penyler/pages/projects.php
@@ -14,9 +14,9 @@ include ('header.html');
 </div>
 <div class="wrapper row3">
   <main class="hoc container clear"> 
-    <div class="center btmspace-80">
+    <div class="center btmspace-50">
       <h2 class="heading">Check out some of my Projects</h2>
-      <p class="nospace">Evaluate my work and see if it is a right fit for you. Below are some simple exercises I did in order to showcase my project development skills. Ass seen I can work with API's, write to and from a stack as seen with Social Network site, and manage large amounts of data.</p>
+      <p class="nospace">Evaluate my work and see if it is a right fit for you. Below are some simple exercises I did in order to showcase my project development skills. As seen I can work with API's, write to and from a stack, and handle large amounts of data by displaying it in differnt forms. The logos are simple logos that I used for my projects - click on each to learn more.</p>
     </div>
     <ul class="nospace group cta">
       <li class="one_half first">
@@ -52,7 +52,9 @@ include ('header.html');
         </article>
       </li>
     </ul>
+  </main>
     <div class="clear"></div>
+    <br>
   </section>
 </div>
 <?php
